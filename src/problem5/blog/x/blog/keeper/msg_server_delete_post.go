a9d7b0672272f6d5bcc706a031fa6c1a3a52e07b
Tan-Jia-Rong <tanjiarongtjr@gmail.com>
2023-10-15 01:40:12 +0800

    Implement update-post

--- src/problem5/blog/x/blog/keeper/msg_server_delete_post.go
+++ src/problem5/blog/x/blog/keeper/msg_server_delete_post.go
@@ -2,16 +2,22 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	"blog/x/blog/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) DeletePost(goCtx context.Context, msg *types.MsgDeletePost) (*types.MsgDeletePostResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
+	val, found := k.GetPost(ctx, msg.Id)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
+	if msg.Creator != val.Creator {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+	k.RemovePost(ctx, msg.Id)
 	return &types.MsgDeletePostResponse{}, nil
 }
